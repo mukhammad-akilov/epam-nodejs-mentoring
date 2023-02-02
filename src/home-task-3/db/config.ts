0a2c2ab3c63b75cfcdaaad8bb438fc1936100fdf
Mukhamadkhon Akilov <mukhamadkhon_akilov@epam.com>
2023-02-02 12:58:44 +0500

    Add: .env file

--- src/home-task-3/db/config.ts
+++ src/home-task-3/db/config.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 // import { Dialect, Sequelize } from 'sequelize';
 
 // const dbName = process.env.DB_NAME as string;
@@ -15,14 +17,14 @@
 
 import { Sequelize, DataTypes } from 'sequelize';
 const sequelizeConnection = new Sequelize(
-  'postgres://sxxxnlwk:BC4JoO--aSeTSbSZgZh2zu12iYdS-hT3@hattie.db.elephantsql.com/sxxxnlwk',
+  `${process.env.DB_TYPE}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
   {
     define: {
       createdAt: 'created_at',
       updatedAt: 'updated_at',
     },
   },
-); // Example for postgres
+);
 
 try {
   await sequelizeConnection.authenticate();
